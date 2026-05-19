b7e2f9f6292f7f0622187523849370f6472ce01a
w25009 <w25009@osfl.ac.jp>
2026-05-19 16:12:26 +0900

    remove nested git

---
+++ my-next-project/app/page.tsx
@@ -0,0 +1,38 @@
+import styles from "./page.module.css"
+import Image from "next/image";
+import {getNewsList}  from "@/app/_libs/microcms";
+import {TOP_NEWS_LIMIT} from "@/app/constants";
+import NewsList from "@/app/components/NewsList";
+import ButtonLink from "@/app/components/ButtonLink";
+
+
+export default async function Home() {
+  const data= await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
+  const name ="世界";
+  return (
+    <>
+    <section className={styles.top}>
+    <div>
+      <h1 className={styles.title}>テクノロジーの力{name}を変える</h1>
+      <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+    </div>
+    <Image
+     className={styles.bgimg}
+    src="/img-mv.jpg" 
+    alt="" 
+    width={4000}
+    height={1200}
+    />
+</section>
+<section className={styles.news}>
+  <h2 className={styles.newsTitle}>News</h2>
+  <NewsList news={data.contents}/>
+  <div className={styles.newsLink}>
+    <ButtonLink href="/news">もっとみる</ButtonLink>
+  </div>
+</section>
+  </>
+  );
+};
