1279fbd630cc6cb2b8d49f229423c14432ffea3b
w25009 <w25009@osfl.ac.jp>
2026-05-20 14:48:47 +0900

    6章まで完了

--- my-next-project/app/components/Date/index.tsx
+++ my-next-project/app/components/Date/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./index.module.css";
+import {formatDate} from "@/app/_libs/utils";
 
 type Props={
     date:string;
@@ -10,7 +11,7 @@ export default function Date({date}:Props){
         <span className={styles.date}>
             <Image src='/clock.svg' alt='clock' width={16} height={16}
             loading='eager'/>
-            {date}
+            {formatDate(date)}
         </span>
     );
 }
