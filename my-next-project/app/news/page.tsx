5f68a8461aa013f9ab41c58db55c5d905883889b
w25009 <w25009@osfl.ac.jp>
2026-06-03 14:37:39 +0900

    ニュースにSSRを適用

--- my-next-project/app/news/page.tsx
+++ my-next-project/app/news/page.tsx
@@ -4,6 +4,8 @@ import Pagination from "@/app/components/Pagination";
 import SearchField from "@/app/components/SearchField";
 import {NEWS_LIST_LIMIT} from "@/app/constants";
 
+export const revalidate=0;
+
 export default async function Page(){
     const {contents:news,totalCount} =await getNewsList({
         limit:NEWS_LIST_LIMIT,
